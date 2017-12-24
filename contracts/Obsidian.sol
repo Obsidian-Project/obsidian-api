75ada1fc7120a4598342b8207d95a60004a2d3b0
Edson Alcalá <edson_alcala@msn.com>
2017-12-24 12:54:02 +0000

    added sample smart contract

---
+++ contracts/Obsidian.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.0;
+contract Obsidian {
+    
+    function Obsidian() public {
+        
+    }
+    
+    function greeting() public pure returns(string result) {
+        result = "Hello world";
+        return result;
+    }
+}
