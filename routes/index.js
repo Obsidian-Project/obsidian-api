01d8f231122d3f7039a73003e83ab47d9995575d
Edson Alcala <edson_alcala@msn.com>
2017-12-28 12:14:57 +0000

    added ipfs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const router = new Router();
 const SMART_CONTRACT_ABI = require('../utils/smartcontract').ABI;
 const SMART_CONTRACT_ADDRESS = require('../utils/smartcontract').ADDRESS;
 
+const Readable = require('stream').Readable
+const ipfsAPI = require('ipfs-api')
+
+// connect to ipfs daemon API server
+// connect to ipfs daemon API server
+//const ipfs = ipfsAPI('https://ipfs.infura.io ', '5001', {protocol: 'http'}) // leaving out the arguments will default to these values
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'}) // leaving out the arguments will default to these values
+
 router.get('/', async (ctx) => {
   ctx.body = 'API Working!';
 })
@@ -14,6 +22,33 @@ router.get('/smartcontract', ctx => {
         address: SMART_CONTRACT_ADDRESS
     };
 })
+
+router.post('/program', ctx => {
+    //to change
+    //get the json object
+    let streamJsonProgram = new Readable();
+    let jsonProgram = ctx.request.body;
+    //stringify the json
+    let jsonProgramString = JSON.stringify(jsonProgram);
+    streamJsonProgram.push(jsonProgramString);
+    streamJsonProgram.push(null); 
+    //push to ipfs and return hash?
+    ipfs.add(streamJsonProgram, (err, result) => {
+        if (err) {
+          throw err
+        }
+        console.log(result)
+        ipfs.cat(result[0].hash, (err, data) => {
+            if (err) {
+              return console.log(err);
+            }
+            
+            console.log("DATA:" + data);
+           });
+      })
+})
+
+
 
 
 module.exports = router;
